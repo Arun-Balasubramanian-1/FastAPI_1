3412d88fb4ab10228a6ea52c88e2eb464f9b5bce
Arun-Balasubramanian-1 <arun.balasubramanian.97@gmail.com>
2024-12-14 13:11:59 +0530

    variable values returned in GET response

--- books.py
+++ books.py
@@ -4,4 +4,14 @@ app = FastAPI()
 
 @app.get("/")
 async def say_hello():
-    return { "message": "Hello Arun!"}
+    return { "message": "Hello Arun!" }
+
+
+BOOKS = [
+    { "author": "JK Rowling", "book": "Harry Potter", "stock_count": 97 },
+    { "author": "Morgan Housel", "book": "Psychology of Money", "stock_count": 25 }
+]
+
+@app.get("/books")
+async def get_books():
+    return { "Books": BOOKS}
