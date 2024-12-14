e9b9def2142c22332ba97246c7442472bfcb1ac5
Arun-Balasubramanian-1 <arun.balasubramanian.97@gmail.com>
2024-12-14 15:52:18 +0530

    GET API - Path Param and Endpoints First Match

--- books.py
+++ books.py
@@ -8,10 +8,22 @@ async def say_hello():
 
 
 BOOKS = [
-    { "author": "JK Rowling", "book": "Harry Potter", "stock_count": 97 },
-    { "author": "Morgan Housel", "book": "Psychology of Money", "stock_count": 25 }
+    { "author": "JK Rowling", "book_name": "Harry Potter", "stock_count": 97 },
+    { "author": "Morgan Housel", "book_name": "Psychology of Money", "stock_count": 25 }
 ]
 
 @app.get("/books")
 async def get_books():
-    return { "Books": BOOKS}
+    return BOOKS
+
+@app.get("/books/my_book")
+async def get_books():
+    return { "book": "MY BOOK." }
+
+@app.get("/books/{book_name}")
+async def get_book(book_name: str):
+    for book in BOOKS:
+        if(book.get("book_name").lower() == book_name.lower()):
+            return book
+
+    return { "error": "book not found", "book": book_name}
