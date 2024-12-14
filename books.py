416c6bf086358bda5355415a727497825bd03ff6
Arun-Balasubramanian-1 <arun.balasubramanian.97@gmail.com>
2024-12-14 17:12:29 +0530

    Both Path and Query Param in same request

--- books.py
+++ books.py
@@ -12,6 +12,7 @@ BOOKS = [
     { "category": "fiction", "author": "JK Rowling", "book_name": "Harry Potter", "stock_count": 97 },
     { "category": "fiction", "author": "Frank Hebert", "book_name": "Dune", "stock_count": 97},
     { "category": "fiction", "author": "George Orwell", "book_name": "Animal Farm", "stock_count": 25 },
+    { "category": "non fiction", "author": "George Orwell", "book_name": "The Road to Wigan Pier", "stock_count": 25 },
     { "category": "non fiction","author": "Morgan Housel", "book_name": "Psychology of Money", "stock_count": 25 },
     { "category": "non fiction","author": "James Clear", "book_name": "Atomic Habits", "stock_count": 25 },
     { "category": "non fiction", "author": "Yuvan Noa Harari", "book_name": "Sapiens", "stock_count": 25}
@@ -42,6 +43,18 @@ async def get_books_by_category(category: Optional[str] = None):
     books = []
     for book in BOOKS:
         if(book.get("category").lower() == category):
+            books.append(book)
+
+    return books
+
+
+
+@app.get("/author/{author}/books")
+async def get_books_by_author_and_category(author: str, category: Optional[str] = None):
+
+    books = []
+    for book in BOOKS:
+        if(((category is None) or (book.get("category").lower() == category)) and (book.get("author").lower() == author.lower())):
             books.append(book)
 
     return books
